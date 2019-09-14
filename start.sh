c54207fb36fb2da40fce41323590d7275f0a3c2d
Vili <vili.hatonen@helsinki.fi>
2019-09-14 21:23:38 +0300

    clean WORKING start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# cd ~/projects/magicmirror
-
-# source venv/bin/activate
-
-# export FLASK_APP=hello.py
-
-# flask run >> log.txt 2>&1 &
-
 python3 /home/pi/projects/magicmirror/hello.py >> log.txt 2>&1 &
 
 exit 0
